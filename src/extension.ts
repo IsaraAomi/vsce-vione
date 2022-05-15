198525927449595a960576c6018e095abb19a9db
Isara Aomi <isara.aomi.87u@gmail.com>
2022-05-15 12:49:24 +0900

    Add basic source files.

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,22 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { RurudoContainer1Provider, RurudoContainer1TreeElement } from './rurudoContainer1Provider';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vsce-rurudo" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vsce-rurudo.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vsce-rurudo!');
+	const rurudoContainer1Provider = new RurudoContainer1Provider();
+	vscode.window.registerTreeDataProvider('rurudoContainer1', rurudoContainer1Provider);
+	const showDisposable = vscode.commands.registerCommand('rurudoContainer1.show', (element: RurudoContainer1TreeElement) => {
+		if (element) {
+			vscode.window.showInformationMessage(`This is ${element.name}`, { modal: true });
+		}
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(
+		showDisposable
+	);
 }
 
 // this method is called when your extension is deactivated
---
+++ src/rurudoContainer1Provider.ts
@@ -0,0 +1,66 @@
+import * as vscode from 'vscode';
+
+export class RurudoContainer1Provider implements vscode.TreeDataProvider<RurudoContainer1TreeElement> {
+
+  private rootElements: RurudoContainer1TreeElement[]
+  constructor() {
+    this.rootElements = this.createElements();
+  }
+
+  getTreeItem(element: RurudoContainer1TreeElement): vscode.TreeItem | Thenable<vscode.TreeItem> {
+    const collapsibleState = element.children.length > 0 ? vscode.TreeItemCollapsibleState.Collapsed : vscode.TreeItemCollapsibleState.None;
+    return new vscode.TreeItem(element.name, collapsibleState);
+  }
+
+  getChildren(element?: RurudoContainer1TreeElement): vscode.ProviderResult<RurudoContainer1TreeElement[]> {
+    return element ? element.children : this.rootElements;
+  }
+
+  /**
+   * @return rootElements
+   */
+  private createElements(): RurudoContainer1TreeElement[] {
+    const parent1 = new RurudoContainer1TreeElement('Item1');
+    ['Item1_1', 'Item1_2'].forEach(name => {
+      parent1.addChild(new RurudoContainer1TreeElement(name));
+    });
+
+    const parent2 = new RurudoContainer1TreeElement('Item2');
+    parent2.addChild(new RurudoContainer1TreeElement('Item2_1'));
+    return [parent1, parent2];
+  }
+}
+
+export class RurudoContainer1TreeElement {
+
+
+  private _children: RurudoContainer1TreeElement[];
+  private _parent: RurudoContainer1TreeElement | undefined | null
+  constructor(
+    public name: string
+  ) {
+    this._children = [];
+  }
+
+  get parent(): RurudoContainer1TreeElement | undefined | null {
+    return this._parent;
+  }
+
+  get children(): RurudoContainer1TreeElement[] {
+    return this._children;
+  }
+
+  addChild(child: RurudoContainer1TreeElement) {
+    child.parent?.removeChild(child);
+    this._children.push(child);
+    child._parent = this;
+  }
+
+  removeChild(child: RurudoContainer1TreeElement) {
+    const childIndex = this._children.indexOf(child);
+    if (childIndex >= 0) {
+      this._children.splice(childIndex, 1);
+      child._parent = null;
+    }
+  }
+}
