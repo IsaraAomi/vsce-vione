d5435629e95dfc354efb60c14670c41ba6d9a905
Isara Aomi <isara.aomi.87u@gmail.com>
2022-05-17 00:44:58 +0900

    Modified to follow security policy: https://code.visualstudio.com/api/extension-guides/webview#content-security-policy

--- src/extension.ts
+++ src/extension.ts
@@ -58,14 +58,35 @@ class ImagesViewProvider implements vscode.WebviewViewProvider {
 		// Get the local path to main script run in the webview, then convert it to a uri we can use in the webview.
 		const scriptUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'main.js'));
 
+		// Use a nonce to only allow a specific script to be run.
+		const nonce = getNonce();
+
 		return `<!DOCTYPE html>
 			<html lang="en">
+			<head>
+				<meta charset="UTF-8">
+				<!--
+					Use a content security policy to only allow loading images from https or from our extension directory,
+					and only allow scripts that have a specific nonce.
+				-->
+				<meta 
+					http-equiv="Content-Security-Policy"
+					content="default-src 'none'; img-src ${webview.cspSource} https:; style-src ${webview.cspSource}; script-src 'nonce-${nonce}';">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			</head>
 			<body>
 				<img id="image_0"/>
-				<script src="${scriptUri}"></script>
+				<script nonce="${nonce}" src="${scriptUri}"></script>
 			</body>
 			</html>`;
 	}
 }
 
-
+function getNonce() {
+	let text = '';
+	const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	for (let i = 0; i < 32; i++) {
+		text += possible.charAt(Math.floor(Math.random() * possible.length));
+	}
+	return text;
+}
