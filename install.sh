4aa05064ab1a3bd95d0221eef5b0f6ee50d3a150
IsaraAomi <isara.aomi.87u@gmail.com>
2022-05-18 12:13:47 +0900

    Modified install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-
-code --install-extension vsce-vione-0.0.6.vsix
+VERSION=$1
+if [ -z ${VERSION} ]; then
+    echo "[ERROR] Specify the version number as the first argument."
+    exit 1
+fi
+code --install-extension vsce-vione-${VERSION}.vsix
